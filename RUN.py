83c529d3a7093a485c618559aaca414818376fab
Nishant-create <74850984+Nishant-create@users.noreply.github.com>
2020-11-29 11:12:40 +0530

    Add files via upload

---
+++ RUN.py
@@ -0,0 +1,26 @@
+print("Hello! what's your name?")
+name = input()
+print("Nice to meet you " + name)
+print("Who are you?")
+print("A.HUMAN B.ALIEN")
+answer = input()
+if(answer == 'HUMAN'):
+    print("Very glad to see a human here")
+    print("What would you like Tea or Coffee")
+    ans = input()
+    if(ans == 'Tea')or(ans == 'tea'):
+        print("Ramukaka 2 chay lana")
+        print('Jaldi')
+    elif(ans == 'Coffee')or(ans == 'coffee'):
+        print("Steve 2 Coffees,please!")
+        print("Fast")
+    else:
+        print("No you have to take something")
+    while(ans != 'Tea')or(ans != 'Coffee'):
+            print("Invalid input plz try again")
+            ans = input()
+elif(answer == 'ALIEN'):
+        print("You Alien Get Out Of Here")
+while(answer != 'HUMAN')or(answer != 'ALIEN'):
+    print('Invalid input plz try again')
+    answer = input()
