25506fc8e35d1999881701b509065c8129d138b4
Chauhans <nishant1357kadyan@gmail.com>
2020-12-06 22:50:08 +0530

    submit change

--- RUN.py
+++ RUN.py
@@ -4,7 +4,7 @@ print("Nice to meet you " + name)
 print("Who are you?")
 print("A.HUMAN B.ALIEN")
 answer = input()
-def fun:
+def fun():
     if(answer == 'HUMAN'):
         print("Very glad to see a human here")
         print("What would you like a Tea or a Coffee")
@@ -17,7 +17,7 @@ def fun:
             print("Fast")
         else:
             print("No you have to take something")
-    while(ans != 'Tea')or(ans != 'Coffee'):
+        while(ans != 'Tea')or(ans != 'Coffee'):
             print("Invalid input plz try again")
             ans = input()
     elif(answer == 'ALIEN'):
