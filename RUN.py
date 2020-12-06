1e1ec8cbc7f42fe2b03dbfed167539bbd21f7ff2
Chauhans <nishant1357kadyan@gmail.com>
2020-12-06 22:27:18 +0530

    grammatical changes

--- RUN.py
+++ RUN.py
@@ -4,22 +4,23 @@ print("Nice to meet you " + name)
 print("Who are you?")
 print("A.HUMAN B.ALIEN")
 answer = input()
-if(answer == 'HUMAN'):
-    print("Very glad to see a human here")
-    print("What would you like Tea or Coffee")
-    ans = input()
-    if(ans == 'Tea')or(ans == 'tea'):
-        print("Ramukaka 2 chay lana")
-        print('Jaldi')
-    elif(ans == 'Coffee')or(ans == 'coffee'):
-        print("Steve 2 Coffees,please!")
-        print("Fast")
-    else:
-        print("No you have to take something")
+def fun:
+    if(answer == 'HUMAN'):
+        print("Very glad to see a human here")
+        print("What would you like a Tea or a Coffee")
+        ans = input()
+        if(ans == 'Tea')or(ans == 'tea'):
+            print("Ramukaka 2 chay lana")
+            print('Jaldi')
+        elif(ans == 'Coffee')or(ans == 'coffee'):
+            print("Steve 2 Coffees,please!")
+            print("Fast")
+        else:
+            print("No you have to take something")
     while(ans != 'Tea')or(ans != 'Coffee'):
             print("Invalid input plz try again")
             ans = input()
-elif(answer == 'ALIEN'):
+    elif(answer == 'ALIEN'):
         print("You Alien Get Out Of Here")
 while(answer != 'HUMAN')or(answer != 'ALIEN'):
     print('Invalid input plz try again')
