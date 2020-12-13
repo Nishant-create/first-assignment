6a38fc7a0704c05f13b116a303d1d304c2adb582
Chauhans <nishant1357kadyan@gmail.com>
2020-12-13 15:53:48 +0530

    created the code of the game short

--- First.py
+++ First.py
@@ -1,68 +1,69 @@
 
 import random 
 
-
-
 numberofattempts = 0
 
+def intro():
+    print("Welcome player")
+    print("Let me introduce you to the game.")
+    print("This is a Guess the Number type game, so the machine will randomly generate a number,")
+    print("you have a limited number of chances to guess the number,the game will give you hints")
+    print("on your every guess, I hope you have understood the game, so go enjoy....")
+    print("Oh! I forgot to ask your name.")
+    print("What's your name???")
 
+    name = input()
 
-print("Welcome player")
-print("Let me introduce you to the game.")
-print("This is a Guess the Number type game, so the machine will randomly generate a number,")
-print("you have a limited number of chances to guess the number,the game will give you hints")
-print("on your every guess, I hope you have understood the game, so go enjoy....")
-print("Oh! I forgot to ask your name.")
+    print("Nice to meet you " + str(name))
+    print("Ready to start the game??")
+    print("Press enter to continue")
+    start = input()
 
-name = input()
+def game():
 
-print("Nice to meet you " + str(name))
-print("Ready to start the game??")
-print("Press enter to continue")
+    print("How many attempts do you want?")
 
-start = input()
+    number = input()
+    number = int(number)
 
-print("How many attempts do you want?")
+    print("Please enter lowest number of range")
 
-number = input()
-number = int(number)
+    smallrange = input()
+    smallrange = int(smallrange)
 
-print("How much range do you want???")
-print("Please enter only the Smallest number of range")
+    print("please enter highest number of range")
 
-smallrange = input()
-smallrange = int(smallrange)
+    highrange = input()
+    highrange = int(highrange)
 
-print("please enter only Highest number of range")
+    guessnumber = random.randint(int(smallrange),int(highrange))
 
-highrange = input()
-highrange = int(highrange)
+    winlose = False
 
-guessnumber = random.randint(int(smallrange),int(highrange))
+    print("Your game starts now, Good Luck ;)")
 
-winlose = False
+    for numberofattempts in range(1,int(number)):
+        guess = input()
+        guess = int(guess)
 
-print("Your game starts now, Good Luck ;)")
+        if(guess > guessnumber):
+            print("You went too High")
 
-for numberofattempts in range(1,int(number)):
-    guess = input()
-    guess = int(guess)
+        elif(guess < guessnumber):
+            print("You went too Low")
 
-    if(guess > guessnumber):
-        print("You went too High")
+        elif(guess == guessnumber):
+            winlose = True
+            break
+    if(winlose==True):
+        print("Good Job, you guessed the number in " + str(numberofattempts) + " attempts")
+        print("Press enter to exit...")
+        roxit = input()
+    else:
+        print("You Loser get out of Here")
+        print("press enter to exit...^~^")
+        toxit = input()
 
-    elif(guess < guessnumber):
-        print("You went too Low")
+intro()
 
-    elif(guess == guessnumber):
-        winlose = True
-        break
-
-if(winlose=True):
-    print("Good Job, you guessed the number in " + str(numberofattempts) + " attempts")
-    print("Press enter to exit...")
-    roxit = input()
-else:
-    print("You Loser get out of Here")
-    print("press enter to exit...^~^")
-    toxit = input()
+game()
