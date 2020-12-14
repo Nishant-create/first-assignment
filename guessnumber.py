d79caedd647c4a945e4fa63ca4e30f7fa45af25b
Nishant-create <nishant1357kadyan@gmail.com>
2020-12-14 19:19:34 +0530

    nothing just commit

--- guessnumber.py
+++ guessnumber.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import random
 
 numberofattempts = 0
@@ -38,46 +37,3 @@ elif(winlose == True):
 
 
 
-
-
-
-
-=======
-import random
-
-numberofattempts = 0
-print("Choose your difficulty")
-
-print("Hello, player What's your name")
-name = input()
-print("nice to meet you " + name)
-
-guessnumber = random.randint(1, 60)
-
-print("Well, " + name + " I was thinking a no. between 1 to 60")
-print("Can you Guess the number????")
-
-winlose = False
-
-for numberofattempts in range(1, 9):
-    number = input()
-    number = int(number)
-    
-    if(number > guessnumber):
-        print("Well you went to high.")
-
-    elif(number < guessnumber):
-        print("Well you went to Low.")
-    elif(number == guessnumber):
-        winlose = True
-        break
-
-if(winlose == False):
-    print("You lose,booooooooooooooooooooooooooooooooooo!")
-    print("press any key to exit")
-    roxit = input()
-elif(winlose == True):
-    print("Good Job!, you guessed the number in " + str(numberofattempts) + " attempts.")
-    print("press enter key to exit")
-    toxit = input()
->>>>>>> 30c22daccd43fd855d014dfacdb39c263e7b7010
