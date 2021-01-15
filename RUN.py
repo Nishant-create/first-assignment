d615212da0502de2e9b6cd0472f384ef525e3598
Nishant-create <nishant1357kadyan@gmail.com>
2021-01-15 18:17:57 +0530

    Newer and better version of RUN.py(under progress)

--- RUN.py
+++ RUN.py
@@ -1,30 +1,20 @@
-def deaf():
-    if(ans == 'Tea')or(ans == 'tea'):
-        print("Ramukaka 2 chay lana")
-        print('Jaldi')
-    elif(ans == 'Coffee')or(ans == 'coffee'):
-        print("Steve 2 Coffees,please!")
-        print("Fast")
-    else:
-         print("No you have to take something")
-print("Hello! what's your name?")
-name = input()
-print("Nice to meet you " + name)
-print("Who are you?")
-print("A.HUMAN B.ALIEN")
-answer = input()
-def fun():
-    if(answer == 'HUMAN'):
-        print("Very glad to see a human here")
-        print("What would you like a Tea or a Coffee")
-        ans = input()    
-    elif(answer == 'ALIEN'):
-        print("You Alien Get Out Of Here")
-    while((ans != 'Tea'and ans != 'Coffee')):
-        print("Invalid input plz try again")
-        ans = input()
-        deaf
-while((answer != 'HUMAN'and answer != 'ALIEN')):
-    print('Invalid input plz try again')
-    answer = input()
-    fun()
+
+import time
+
+print("Hello World again.")
+time.sleep(2)
+
+print("See who's here!, actually Don't know who you are?")
+time.sleep(1)
+
+print("Hii, me [BoT] and you??")
+name = input(str)
+print("Hmmmm....")
+time.sleep(3)
+
+print("Nice name " + name )
+print("What do you Like Tea or coffee")
+desire = input()
+
+    
+    
