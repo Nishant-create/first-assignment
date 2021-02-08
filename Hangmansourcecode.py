ebd27654303961a0cfc0f4b9702921276263ede0
Nishant-create <nishant1357kadyan@gmail.com>
2021-02-08 23:28:14 +0530

    New game

---
+++ Hangmansourcecode.py
@@ -0,0 +1,29 @@
+import random
+import sys
+import time
+
+def Delaybetweencharacters():
+    from random import randrange
+
+    global text
+    
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        seconds = "0." + str(randrange(1000, 4000, 1000))
+        seconds = float(seconds)
+        time.sleep(seconds)
+
+def IntroFunction():
+    global text
+    text = ("""Hey, its game time.
+    today we have planned to play a game known as HANGMAN. Its a bit high level game from our Guessnumber Game.
+    So,get ready. In this game you have to guess a word letter by letter. \nNow the interesting thing starts, the word is randomly generated and if you can't guess he word until the hanging man is completely drawn.
+    \nDon't worry you can select difficulty on easy there will be Four to three letter words\nOn intermediate the words change to Five to Six words \nAnd on Hard these will change to Seven letter words.\n\n\nGooD LucK    """)
+
+
+
+
+
+IntroFunction() 
+Delaybetweencharacters()
