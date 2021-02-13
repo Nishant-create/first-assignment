31507e8fdc8ffc88a9794efd43a96fbc13ccb22c
Nishant-create <nishant1357kadyan@gmail.com>
2021-02-13 20:38:54 +0530

    Done the all printing stuff for intro still lacking the main objective of game

--- Hangmansourcecode.py
+++ Hangmansourcecode.py
@@ -1,19 +1,34 @@
 import time 
 import random
 
+Hangman_ascii = ["""  
++=========+
+         |
+         |
+         |
+         |
+         |
+         |
+         |
+         |
+         |
+         |
+        ===""","""
+        """
+
 def IntroFunction():
 
-    text = ("""\n Hey! its Game Time,\nToday we are going to play a game known as HANGMAN. Its a bit high level game from our Guessnumber game.\nIn this game you have to guessnumber a word letter by letter, a hanging an will be drawn side by side on your every incorrect guess.\nand you will lose when the hanging man is completely drawn unless you have guessed the word.\nSo, GooD Luck and enjoy the game """)
+    text = ("""\n Hey! its Game Time,\nToday we are going to play a game known as HANGMAN. Its a bit high level game from our Guessnumber game.\nIn this game you have to guess a word letter by letter, a hanging man will be drawn side by side on your every incorrect guess.\nand you will lose when the hanging man is completely drawn unless you have guessed the word.\nSo, GooD Luck and enjoy the game """)
     
     for c in text:
-        sec = "0." + str(random.randrange(1, 3, 1))
+        sec = "0.0" + str(random.randrange(1, 3, 1))
         sec = float(sec)
         print(c,end=''),time.sleep(sec)
 
     pri = ("""\nSo, lets start with some basic game things.
     What's your name ?:-""")
     for c in pri:
-        sec = "0." + str(random.randrange(1, 3, 1))
+        sec = "0.0" + str(random.randrange(1, 3, 1))
         sec = float(sec)
         print(c,end=''),time.sleep(sec)    
 
@@ -28,9 +43,11 @@ def IntroFunction():
 
 def SettingsFunction():
     
-    text = "\nChosse Difficulty:-\n(a) Easy\n(b) Intermediate\n(c) Hard\n(d) Pro\n(e) Master\n\nNote:- Please write only a single word like a,b,c,d,e. Don't write brackets or the full name of difficulty"
+    text = """
+    Choose Difficulty:-\n(a) Easy\n(b) Intermediate\n(c) Hard\n(d) Pro\n(e) Master\n
+    Note:- Please write only a single word like a,b,c,d,e. Don't write brackets or the full name of difficulty\n"""
     for c in text:
-        sec = "0." + str(random.randrange(1, 5, 1))
+        sec = "0.0" + str(random.randrange(1, 5, 1))
         sec = float(sec)
         print(c,end=''),time.sleep(sec)
 
@@ -48,9 +65,9 @@ def SettingsFunction():
         print('done')
 
     else:
-        prit = "Error.unknown please read the note written with the question."
-        for c in print:
-            sec = "0." + str(random.randrange(1, 5, 1))
+        prit = "((Error)Invalid input) please read the note written with the question."
+        for c in prit:
+            sec = "0.0" + str(random.randrange(1, 5, 1))
             sec = float(sec)
             print(c,end=''),time.sleep(sec)
 
@@ -63,7 +80,15 @@ def LoopFunction():
     while(answer != 'a' and answer != 'b' and answer != 'c' and answer != 'd' and answer != 'e'):
         SettingsFunction() 
 
+def EasyFunction():
+    text = """So, let's start the game"""
+    for c in text:
+        sec = "0.0" + str(random.randrange(1, 5, 1))
+        sec = float(sec)
+        print(c,end='',time.sleep(sec))
 
+    wordslist = ['cape','ant','fun','run','man','rush','ruby','four','two','one','rub','gun','lion','wolf','pig','cow','sit','fit','lit','knit','shot','mop','lot','node','word','pass','top','down','cold','net','wet','rat','mop','vote']
+    print(random.choice())
     
 
 IntroFunction()
--- try.py
+++ try.py
@@ -1,9 +1,4 @@
-# try
-import time
 import random
 
-text = "Hello, this is a test ."
-for char in text:
-    sec = "0." + str(random.randrange(1, 4, 1))
-    sec = float(sec)
-    print (char,end='I am at end'),time.sleep(sec)
+lists = ['a','ab','abc','abcd']
+print(random.choice(lists))
