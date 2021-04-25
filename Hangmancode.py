dc3cc7325bf2c2c05034caa7d787317c77f4177e
Nishant-create <nishant1357kadyan@gmail.com>
2021-04-25 18:57:57 +0530

    Now complete(Bug fixes)

--- Hangmancode.py
+++ Hangmancode.py
@@ -5,7 +5,7 @@ import string
 def winnerorlosercheckfunction():
     global winlose
     if (winlose == True):
-        text = """The Game is complete. You have Guessed the letter and the HANGMAN isn't still completed.\
+        text = """\n\n\nThe Game is complete. You have Guessed the letter and the HANGMAN isn't still completed.\
              ___________
             '._==_==_=_.'
             .-\:      /-.
@@ -22,9 +22,7 @@ def winnerorlosercheckfunction():
         texty = "Oh! Looks like the Hangman has been completed.😞😞 Better Luck Next Time."    
         slowprint(texty)
 
-def hangman():
-    global hangman_ascii
-    hangman_ascii = ['''
+hangman_ascii = ['''
     +=========+
              |
              |
@@ -116,19 +114,19 @@ def introfunction():
     # This Function will let the player know that what program even is this. 
     text = ("""\n Hey! its Game Time,\nToday we are going to play a game known as HANGMAN. Its a bit high level game from our Guessnumber game.\nIn this game you have to guess a word letter by letter, a hanging man will be drawn side by side on your every incorrect guess.\nand you will lose when the hanging man is completely drawn unless you have guessed the word.\nSo, GooD Luck and enjoy the game """)
     
-    slowPrint(text)
+    slowprint(text)
 
     speech = ("""\nSo, lets start with some basic game things.
     What's your name ?:-""")
     # you will see it after every "string" variable.
-    slowPrint(speech)  
+    slowprint(speech)  
 
     global name
     name = input()
     
     string = "Choose your language:-"
     slowprint(string)
-    time.sleep(1)
+    time.sleep(5)
     troll =  "Just kidding\nI only know English "
     slowprint(troll)
 
@@ -136,7 +134,7 @@ def settingsfunction():
     text = """
     Choose Difficulty:-\n(a) Easy\n(b) Intermediate\n(c) Hard\n(d) Pro\n(e) Master\n
     Note:- Please write full name of difficulty.\n"""
-    slowPrint(text)
+    slowprint(text)
 
     global answer
     answer = input(str)
@@ -149,7 +147,6 @@ def loopfunction():
 
 def OBJfunction():
 
-    global hangman_ascii
     global answer
 
     easy_wordslist = ['cap','ant','fun','run','man','rub','fur','two','one','gun','pig','cow','sit','fit','lit','kit','hot','mop','lot','war','top','don','cod','net','wet','rat','mop']
@@ -158,28 +155,28 @@ def OBJfunction():
     pro_wordslist = ["Friend","twelve","eleven","common","visual","studio","letter","source","winner","number","output","abroad","access","eagles","broken","bureau","advise","belief","gender","golden","guilty","powers","german","france","joseph","handle","yellow","walker","kadyan","inaaya","versus","unless","travel","potter","ronald","useful","exceed","extent","fright","crisis","gaming","defend"]
     master_wordslist = ['acquire','absence','scissor','billion','million','privacy','private','command','windows','control','silence','smoking','butcher','poacher','someone','welcome','waiting','victory','venture','problem','console','station','outline','setting','editors','unsaved','silicon','phoenix','product','nursing','network','obvious','killing','kitchen','illegal','warrior','exactly','consent','crystal','chicken','chronic','balance','bedroom','already','outpost','masters','classes','captain','compete','economy','diamond','palette']
 
-    if answer = 'easy':
+    if (answer == 'easy'):
         selectedword = (random.choice(easy_wordslist))
         emptystring = "***"
 
-    if answer = 'intermediate':
+    if (answer == 'intermediate'):
         selectedword = (random.choice(intermediate_wordslist))
         emptystring = "****"
  
-    elif answer = 'hard':
+    elif (answer == 'hard'):
         selectedword = (random.choice(hard_wordslist))
         emptystring = "*****"
 
-    elif answer = 'pro':
+    elif (answer == 'pro'):
         selectedword = (random.choice(pro_wordslist))
         emptystring = "******"
 
-    elif answer = 'master':
+    elif (answer == 'master'):
         selectedword = (random.choice(master_wordslist))
         emptystring = "*******"
 
     text = "So, let's start your game."
-    slowPrint(text)
+    slowprint(text)
     print('\n\n')
     slowprint(emptystring)
     printstatement = "\n\n\nGuess a letter\n "
@@ -192,18 +189,20 @@ def OBJfunction():
     
     for numberofguesses in range(1, 1024):
         guess = input(str)
-        guess = guess.lower
+        guess = guess.lower()
 
         inspector = selectedword.find(guess)
-
+        inspector = int(inspector)
         if inspector == int(-1):
+            wronganswerlist = ["Wrong guess","nononononono!","Nope this is not the right answer","Error not the input required","Its not that easy man!","Hey! you, don't give wrong answers "]
             wronganswervariable = (random.choice(wronganswerlist)),
             slowprint(wronganswervariable)
+            
             hangman_printer = ((hangman_ascii[numberofwrongguesses]) + '\n')
             slowprint(hangman_printer)
             numberofwrongguesses = numberofwrongguesses + 1
 
-            if numberofwrongguesses = len(hangman_ascii):
+            if numberofwrongguesses == len(hangman_ascii):
                 winlose = False
                 break
 
@@ -232,11 +231,11 @@ def mainfunction():
 def loopFunction():
 
     text = "Do you wanna play it again?\nNote : Only 'yes' or 'no'."
-
+    slowprint(text)
     canihavetheanswer = input(str)
     canihavetheanswer = canihavetheanswer.lower()
 
-    if canihavetheanswer = 'yes':
+    if canihavetheanswer == 'yes':
         printvariable = "Ok then"
         slowprint(printvariable)
         mainfunction()
@@ -246,3 +245,4 @@ def loopFunction():
 
 introfunction()
 mainfunction()
+loopFunction()
