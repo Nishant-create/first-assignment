aa66ed0e9c4fe232d797364e3a3df3fb14713726
Nishant-create <nishant1357kadyan@gmail.com>
2021-07-14 23:48:14 +0530

    It has a PyGame program in it

--- Test.py
+++ Test.py
@@ -1,12 +1,26 @@
-import pygame, sys
+import pygame
 from pygame.locals import *
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Hello World!')
+
+white = (255,255,255)
+
+height = 400
+width = 500
+
+display_surface = pygame.display.set_mode((width,height))
+pygame.display.set_caption('image')
+
+image = pygame.image.load(r'F:/Images and documents and other things/Images/download.jpg')
+
 while True:
-    for event in piu        ygame.event.get():
-        if event.type == QUIT:
+    display_surface.fill(white)
+    display_surface.blit(image,(0,0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+            quit()
+
     pygame.display.update()
+    
