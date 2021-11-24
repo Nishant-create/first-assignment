c9b04bfaad13a1b45cf6c5a3281861f9ff6b868a
Nishant-create <nishant1357kadyan@gmail.com>
2021-11-24 21:16:31 +0530

    commit

--- Test.py
+++ Test.py
@@ -1,26 +1,2 @@
-import pygame
-from pygame.locals import *
-
-pygame.init()
-
-white = (255,255,255)
-
-height = 400
-width = 500
-
-display_surface = pygame.display.set_mode((width,height))
-pygame.display.set_caption('image')
-
-image = pygame.image.load(r'F:/Images and documents and other things/Images/download.jpg')
-
-while True:
-    display_surface.fill(white)
-    display_surface.blit(image,(0,0))
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-
-    pygame.display.update()
-    
+print('its just simply a text\r')
+print('nothing to worry about')
